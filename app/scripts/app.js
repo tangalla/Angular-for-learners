a15e089d520f90858b558ca963a9fc712cbfa511
Manju Tangalla <mtangalla@opencrowd.com>
2015-10-16 11:51:30 -0400

    7

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularForLearnersApp
+ * @description
+ * # angularForLearnersApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularForLearnersApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
